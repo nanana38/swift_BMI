c149ed5bafb12fc07f747da1a377149b151703af
nanana38 <hyeoni_yoo@naver.com>
2022-07-13 10:36:20 +0900

    [BMI] second_commt

---
+++ BMI/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  BMI
+//
+//  Created by 유현이 on 2022/07/13.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var bmiNumberLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
+    var bmi: Double?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        makeUI()
+        
+    }
+    
+    func makeUI() {
+        bmiNumberLabel.clipsToBounds = true
+        bmiNumberLabel.layer.cornerRadius = 8
+        bmiNumberLabel.backgroundColor = .gray
+        
+        backButton.clipsToBounds = true
+        backButton.layer.cornerRadius = 5
+        
+        guard let bmi = bmi else { return }
+        bmiNumberLabel.text = String(bmi)
+    }
+
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -9,11 +9,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        makeUI()
+        
+    }
+    
+    func makeUI() {
+        heightTextField.delegate = self
+        weightTextField.delegate = self
+        
+        mainLabel.text = "키와 몸무게를 입력해 주세요."
+        calculateButton.clipsToBounds = true
+        calculateButton.layer.cornerRadius = 5
+        calculateButton.setTitle("BMI 계산하기", for: .normal)
+        heightTextField.placeholder = "cm단위로 입력해주세요."
+        weightTextField.placeholder = "kg단위로 입력해주세요."
+    }
+
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if heightTextField.text == "" || weightTextField.text == "" {
+            mainLabel.text = "키와 몸무게를 입력하셔야 합니다."
+            mainLabel.textColor = UIColor.red
+            return false
+        }
+        mainLabel.text = "키와 몸무게를 입력해주세요."
+        mainLabel.textColor = UIColor.black
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC" {
+            let secondVC = segue.destination as! SecondViewController
+            
+        }
+        // 다음 화면으로 가기전에 텍스트필드 비우기
+        heightTextField.text = ""
+        weightTextField.text = ""
+        
+        
     }
 
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if Int(string) != nil || string == "" {
+            return true
+        }
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if heightTextField.text != "", weightTextField.text != "" {
+            weightTextField.resignFirstResponder()
+            return true
+        } else if heightTextField.text != "" {
+            weightTextField.becomeFirstResponder()
+            return true
+        }
+        return false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        heightTextField.resignFirstResponder()
+        weightTextField.resignFirstResponder()
+    }
+}
